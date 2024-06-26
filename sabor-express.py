52bf703319129eb0b99c7b7e0549e0919cbf9703
Nicholas Prade <nicholas.prade@gmail.com>
2024-06-26 19:53:17 -0300

    m

--- sabor-express.py
+++ sabor-express.py
@@ -1,29 +1,42 @@
 import os
 
-print("""
-█─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
-█▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
-▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
-""")
+def show_banner():
+    print("""
+    █─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
+    █▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
+    ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
+    """)
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurantes')
-print('3. Ativar Restaurante')
-print('4. Sair')
+def show_menu():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurantes')
+    print('3. Ativar Restaurante')
+    print('4. Sair')
 
-menu_option = int(input('\nEscolha uma opção: '))
-print(f'Você escolheu a opção {menu_option}')
+def choose_option():
+    menu_option = int(input('\nEscolha uma opção: '))
+    
+    if menu_option == 1:
+        os.system('cls')
+        print('Cadastrar Restaurante')
+    elif menu_option == 2:
+        os.system('cls')
+        print('Listar Restaurantes')
+    elif menu_option == 3:
+        os.system('cls')
+        print('Ativar Restaurante')
+    else:
+        close_app()
 
 def close_app():
     os.system('cls')
-    print('Até Logo!')
+    print('Até Logo!\n')
 
-if menu_option == 1:
-    print('Cadastrar Restaurante')
-elif menu_option == 2:
-    print('Listar Restaurantes')
-elif menu_option == 3:
-    print('Ativar Restaurante')
-else:
-    close_app
-    
+def main():
+    show_banner()
+    show_menu()
+    choose_option()
+
+
+if __name__ == '__main__':
+    main()
