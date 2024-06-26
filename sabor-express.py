a38f1ad2d4063b33ef765239c73c12269f40f702
Nicholas Prade <nicholas.prade@gmail.com>
2024-06-26 19:35:20 -0300

    .

--- sabor-express.py
+++ sabor-express.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 █─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
 █▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
@@ -5,9 +7,19 @@ print("""
 """)
 
 print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
+print('2. Listar Restaurantes')
 print('3. Ativar Restaurante')
 print('4. Sair')
 
-menu_option = input('\nEscolha uma opção: ')
+menu_option = int(input('\nEscolha uma opção: '))
 print(f'Você escolheu a opção {menu_option}')
+
+if menu_option == 1:
+    print('Cadastrar Restaurante')
+elif menu_option == 2:
+    print('Listar Restaurantes')
+elif menu_option == 3:
+    print('Ativar Restaurante')
+else:
+    os.system('cls')
+    print('Até Logo!')
