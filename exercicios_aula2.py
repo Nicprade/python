fac4dfd9ed300d64e504f8b43f5cd425ca15b2b4
Nicholas Prade <nicholas.prade@gmail.com>
2024-06-27 13:33:49 -0300

    final version

--- exercicios_aula2.py
+++ exercicios_aula2.py
@@ -1,84 +1,84 @@
 import os
 
 def exercicio_1():
-    print('\n -- EXERCÍCIO 1 --\n')
-    print('Exercicio 1: Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.\n\n')
+    print('\n-- EXERCÍCIO 1 --\n')
+    print('Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.\n\n')
     
-    number = int(input('Digite o Número:'))
+    number = int(input('Digite o Número: '))
   
     if number % 2 == 0:
-        os.system('cls')
         print(f'O número {number} é par.')
     else:
-        os.system('cls')
         print(f'O número {number} é ímpar.')
 
 def exercicio_2():
-    print('\n -- EXERCÍCIO 2 --')
+    print('\n-- EXERCÍCIO 2 --')
     print("""
-    Exercício 2: Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:
-    - Criança: 0 a 12 anos;
-    - Adolescente: 13 a 18 anos;
-    - Adulto: acima de 18 anos.
+Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:
+- Criança: 0 a 12 anos;
+- Adolescente: 13 a 18 anos;
+- Adulto: acima de 18 anos.
     \n""")
 
-    age = int(input('Digite sua idade:'))
+    age = int(input('Digite sua idade: '))
 
     if 0 <= age <= 12:
-        os.system('cls')
-        print(f'Você é uma CRIANÇA de {age} anos')
+        print(f'Você é uma CRIANÇA')
     elif 13 <= age <= 18:
-        os.system('cls')
-        print(f'Você é um ADOLESCENTE de {age} anos')
+        print(f'Você é um ADOLESCENTE')
     else:
-        os.system('cls')
-        print(f'Você é um ADULTO de {age} anos')
+        print(f'Você é um ADULTO')
 
 def exercicio_3():
-    print('\n -- EXERCÍCIO 3 --\n')
-    print('Exercício 3: Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.\n\n')
+    print('\n-- EXERCÍCIO 3 --\n')
+    print('Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.\n')
     
-    USER = nicholas.prade
-    PASSWORD = abcd12345
+    USER = 'nicholas.prade'
+    PASSWORD = 'abcd12345'
 
-    test_user = input('Usuário:')
-    test_password = input('Senha:')
+    print (f"""para conferencia:
+usuário = {USER}
+senha = {PASSWORD}
+    """)
+
+    test_user = input('Usuário: ')
+    test_password = input('Senha: ')
 
     if test_user == USER and test_password == PASSWORD:
-        print('ACESSO AUTORIZADO')
+        print('\nACESSO AUTORIZADO')
     else:
         print('\nUSUÁRIO ou SENHA incorretos')
 
 def exercicio_4():
-    print('\n -- EXERCÍCIO 4 --')
+    print('\n-- EXERCÍCIO 4 --')
     print("""
-    Exercício 4: Solicite ao usuário as coordenadas (x, y) de um ponto qualquer e utilize uma estrutura if elif else para determinar em qual quadrante do plano cartesiano o ponto se encontra de acordo com as seguintes condições:
-    - Primeiro Quadrante: os valores de x e y devem ser maiores que zero;
-    - Segundo Quadrante: o valor de x é menor que zero e o valor de y é maior que zero;
-    - Terceiro Quadrante: os valores de x e y devem ser menores que zero;
-    - Quarto Quadrante: o valor de x é maior que zero e o valor de y é menor que zero;
-    - Caso contrário: o ponto está localizado no eixo ou origem.
+Solicite ao usuário as coordenadas (x, y) de um ponto qualquer e utilize uma estrutura if elif else para determinar em qual quadrante do plano cartesiano o ponto se encontra de acordo com as seguintes condições:
+- Primeiro Quadrante: os valores de x e y devem ser maiores que zero;
+- Segundo Quadrante: o valor de x é menor que zero e o valor de y é maior que zero;
+- Terceiro Quadrante: os valores de x e y devem ser menores que zero;
+- Quarto Quadrante: o valor de x é maior que zero e o valor de y é menor que zero;
+- Caso contrário: o ponto está localizado no eixo ou origem.
     \n""")
 
-    number_x = int(input('Digite o valor de X:'))
-    number_y = int(input('Digite o valor de Y:'))
+    number_x = int(input('Digite o valor de X: '))
+    number_y = int(input('Digite o valor de Y: '))
     
-    if number_x < 0 and number_y > 0:
+    if number_x > 0 and number_y > 0:
         print('\nEsse ponto está no PRIMEIRO QUADRANTE')
         print("""
                 |
-          aqui  |
+                |  aqui
                 |
         -------- --------
                 |
                 |
                 |
         """)
-    elif number_x > 0 and number_y > 0:
+    elif number_x < 0 and number_y > 0:
         print('\nEsse ponto está no SEGUNDO QUADRANTE')
         print("""
                 |
-                |  aqui
+          aqui  |  
                 |
         -------- --------
                 |
@@ -167,24 +167,30 @@ def exercicio_4():
 
 
 def main ():
-  print ('\nHora da prática: condicionais\n')
-  choose_test = int(input('Qual exercício você quer testar (1 - 4):'))
+    print ('\nHora da prática: condicionais')
+    print ("""
+1. Par ou ímpar?
+2. Criança, adolescente ou adulto?
+3. Usuário e Senha
+4. Plano cartesiano
+""")
+    choose_test = int(input('Qual exercício você quer testar (1 - 4): '))
 
-  match choose_test:
-      case 1:
-        os.system('cls')
-        exercicio_1()
-      case 2:
-        os.system('cls')
-        exercicio_2()
-      case 2:
-        os.system('cls')
-        exercicio_2()
-      case 4:
-        os.system('cls')
-        exercicio_4()
-      case _:
-        print('OPÇÃO INVÁLIDA')
+    match choose_test:
+        case 1:
+            os.system('cls')
+            exercicio_1()
+        case 2:
+            os.system('cls')
+            exercicio_2()
+        case 3:
+            os.system('cls')
+            exercicio_3()
+        case 4:
+            os.system('cls')
+            exercicio_4()
+        case _:
+            print('OPÇÃO INVÁLIDA')
 
 if __name__ == '__main__':
     main()
