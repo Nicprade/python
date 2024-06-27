801b01418a308d2e1eb0feb7034d1e2d8ae69ab1
Nicholas Prade <45038874+Nicprade@users.noreply.github.com>
2024-06-27 11:08:22 -0300

    Create exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,21 @@
+import os
+
+# Exercicio 1: Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.
+def exercicio_1():
+  number = int(input('Digite o Número:'))
+  
+  if number % 2 = 0:
+    os.system('cls')
+    print(f'O número {number} é par.')
+  else:
+    os.system('cls')
+    print(f'O número {number} é ímpar.')
+
+
+# Evercício 2:  Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:
+# Criança: 0 a 12 anos;
+# Adolescente: 13 a 18 anos;
+# Adulto: acima de 18 anos.
+
+def exercicio_2 ():
+  age = int(input('Digite sua idade:'))
