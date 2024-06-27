ee0e02f34b78611b12fd7c00c11bf766d62d15b5
Nicholas Prade <45038874+Nicprade@users.noreply.github.com>
2024-06-27 11:16:26 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -2,20 +2,32 @@ import os
 
 # Exercicio 1: Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.
 def exercicio_1():
-  number = int(input('Digite o Número:'))
+    number = int(input('Digite o Número:'))
   
-  if number % 2 = 0:
-    os.system('cls')
-    print(f'O número {number} é par.')
-  else:
-    os.system('cls')
-    print(f'O número {number} é ímpar.')
+    if number % 2 = 0:
+        os.system('cls')
+        print(f'O número {number} é par.')
+    else:
+        os.system('cls')
+        print(f'O número {number} é ímpar.')
 
-
-# Evercício 2:  Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:
+# Exercício 2: Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:
 # Criança: 0 a 12 anos;
 # Adolescente: 13 a 18 anos;
 # Adulto: acima de 18 anos.
+def exercicio_2():
+    age = int(input('Digite sua idade:'))
 
-def exercicio_2 ():
-  age = int(input('Digite sua idade:'))
+    if 0 <= age <= 12:
+        os.system('cls)
+        print(f.'Você é uma CRIANÇA de {age} anos')
+    elif 13 <= age <= 18:
+        os.system('cls)
+        print(f.'Você é um ADOLESCENTE de {age} anos')
+    else:
+        os.system('cls)
+        print(f.'Você é um ADULTO de {age} anos')
+
+# Exercício 3: Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.
+
+def exercicio_3():
